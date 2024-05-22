5c0e7f5fd204b1377f67704b4864822beedfa000
Georgi Petkov <george18petkov@gmail.com>
2024-05-22 14:07:10 +0300

    Implement unit of work

---
+++ course-work/Implementations/InvestmentManagerApi/InvestmentManagerApi.Data/Repositories/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InvestmentManagerApi.Data.Repositories.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        DbContext Context { get; }
+
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ course-work/Implementations/InvestmentManagerApi/InvestmentManagerApi.Data/Repositories/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using InvestmentManagerApi.Data.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace InvestmentManagerApi.Data.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext context;
+
+        public DbContext Context 
+        {
+            get { return this.context; }
+        }
+
+        public UnitOfWork(DbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Dispose() => this.context?.Dispose();
+
+        public async Task<int> SaveChangesAsync() => await this.context.SaveChangesAsync();
+    }
+}
